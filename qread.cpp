caad76f1d5a661140715a950bedffe1f672b3022
GuChengGordon <111498044+GuChengGordon@users.noreply.github.com>
2022-09-13 07:33:06 +0000

    Create qread.cpp

---
+++ qread.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int read()
+{
+	int x = 0, f = 1;
+	char ch;
+	ch = getchar();
+	while (!isdigit(ch) && ch != '-')
+	{
+		ch = getchar();
+	}
+	if (ch == '-')
+		f = -1, ch = getchar();
+	while (isdigit(ch))
+	{
+		x = x * 10 + ch - '0';
+		ch = getchar();
+	}
+	return x * f;
+}
+
+void write(int x)
+{
+	if (x < 0)
+	{
+		putchar('-');
+		write(-x);
+		return;
+	}
+	if (x < 10)
+	{
+		putchar(x + '0');
+		return;
+	}
+	write(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int main()
+{
+
+ 	return 0;
+}
+
